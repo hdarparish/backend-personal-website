dbdd4c0e788006625d618552cfe18de0b1258d2b
Darparish <HDarparish@corp.iacna.com>
2020-08-18 15:17:46 -0400

    add callback function

--- src/db.js
+++ src/db.js
@@ -1,12 +1,122 @@
 import { promises as fs, write } from 'fs'
 import path from 'path'
 import dotenv from 'dotenv'
+import * as jwtoken from 'jsonwebtoken'
+
+import argon2 from 'argon2'
+
+import mysql from "mysql";
+import { response } from 'express'
 
 dotenv.config();
 
 const entriesPath = path.resolve(process.env.DB_ENTRIES_LOCATION);
 const usersPath = path.resolve(process.env.DB_USERS_LOCATION);
 let filePath;
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "nodeclient",
+    password: "123456",
+    database: "portfolio",
+});
+
+// open the MySQL connection
+db.connect((error) => {
+    if (error) throw error;
+    console.log("Successfully connected to the database.");
+});
+
+const addEntries = async (reqBody) => {
+    let id = reqBody.id;
+    let name = reqBody.name;
+    let email = reqBody.email;
+    let phoneNumber = reqBody.phoneNumber;
+    let comment = reqBody.content;
+    try {
+        let query = `INSERT INTO contact_entries values (?, ?, ?, ?, ?) `
+        db.query(query, [id, name, email, phoneNumber, comment], (err, result) => {
+            if (err) {
+                throw err;
+            }
+            console.log("request added to database")
+        });
+    }
+    catch (err) {
+        console.error(err);
+    }
+
+}
+
+const userLogin = async (reqBody, callback) => {
+    let email = reqBody.email;
+    let password = reqBody.password;
+
+    try {
+        let query = "Select * from users where email = ?";
+
+        db.query(query, [email], async (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            if (typeof result !== 'undefined' && result) {
+                //do stuff if query is defined and not null
+                //return response.status(401).send({ message: "incorrect credentials provided" })
+                if (result.length > 0) {
+                    let passwordValid = await argon2.verify(result[0].password, password);
+                    if (passwordValid) {
+
+                        const token = jwtoken.sign({ email }, process.env.JWT_SECRET, { expiresIn: '10m' })
+                        return callback(token);
+
+                    }
+                }
+            }
+            return callback(null);
+        });
+    }
+    catch (err) {
+        console.error(err);
+        next(err);
+    }
+}
+
+const addUser = async (reqBody) => {
+    let id = reqBody.id;
+    let name = reqBody.name;
+    let email = reqBody.email;
+    let password = reqBody.password;
+
+    try {
+        let query = "INSERT INTO users VALUES (?, ?, ?, ?)";
+        db.query(query, [id, name, password, email], (err, result) => {
+            if (err) throw err;
+
+            return result;
+        });
+    }
+    catch (err) {
+        console.error(err);
+        // next(err);
+    }
+}
+
+const getEntries = async (callback) => {
+    let query = `SELECT * FROM contact_entries`
+    try {
+        db.query(query, (err, result) => {
+            return callback(result);
+        });
+    }
+    catch (err) {
+        console.error(err);
+        // next(err);
+    }
+
+
+}
 
 const getData = async (reqFile) => {
     let content;
@@ -17,8 +127,8 @@ const getData = async (reqFile) => {
     }
     catch (err) {
         //check if the error code is caused by the missing file(s), then call the function to write/create the JSON file
-        if(err.code === "ENOENT"){
-            console.log("File Doesn't exist")         
+        if (err.code === "ENOENT") {
+            console.log("File Doesn't exist")
             content = [];
             await writeData(content)
             console.log("File Created")
@@ -59,6 +169,10 @@ const addData = async (data, reqFile) => {
 
 export {
     getData,
-    addData
+    addData,
+    addEntries,
+    userLogin,
+    addUser,
+    getEntries
 }
 
